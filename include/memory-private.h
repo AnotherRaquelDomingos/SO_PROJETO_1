63ba09ae444091919ecd41ed0687125e0e05ff4e
AnotherRaquelDomingos <raq.domingos@gmail.com>
2022-04-10 19:48:47 +0100

    Memory-private com nomes e numeros, zip tambem atualizado

--- include/memory-private.h
+++ include/memory-private.h
@@ -1,4 +1,11 @@
 /*
+* Grupo nº: 25
+* Afonso Santos - FC56368
+* Alexandre Figueiredo - FC57099
+* Raquel Domingos - FC56378
+*/
+
+/*
 * Função que conta o número de dígitos de um número inteiro.
 */
 int numDigits(int);
